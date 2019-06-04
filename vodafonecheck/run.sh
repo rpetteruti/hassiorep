1d6ffa6edbb64774e0e381e6eddc3604fa3386e6
rpetteruti <raffaele.petteruti@gmail.com>
2019-06-04 22:15:36 +0200

    Update run.sh

--- vodafonecheck/run.sh
+++ vodafonecheck/run.sh
@@ -6,7 +6,7 @@ CONFIG_PATH=/data/options.json
 USERNAME=$(jq --raw-output ".username" $CONFIG_PATH)
 PASSWORD=$(jq --raw-output ".password" $CONFIG_PATH)
 
-echo "Parameters: $USERNAME@$PASSWORD"
+echo "Parameters: $USERNAME $PASSWORD"
 
 while true; do 
   if ping -c 5 8.8.8.8 &> /dev/null
@@ -14,12 +14,7 @@ while true; do
     echo "Internet Ok"
   else
     echo "Internet KO, starting vodafone autologin"
-    #python vodafone.py
-    URL=$(curl -Ls -w %{url_effective} http://google.com)
-    URL=${URL:0:47}login${URL:53:165}
-    echo "$URL"
-    DATA=chooseCountry=VF_IT%2F&userFake=${USERNAME}&UserName=VF_IT%2F${USERNAME}&Password=${PASSWORD}&rememberMe=true&_rememberMe=on
-    curl --data "${DATA}" ${URL}
+    python vodafone.py $USERNAME $PASSWORD
   fi
   sleep 30
 done
