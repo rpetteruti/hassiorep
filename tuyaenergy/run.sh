03198625b1c825538656382a53bcd8e0d760b902
rpetteruti <raffaele.petteruti@gmail.com>
2020-10-11 12:32:02 +0200

    Update run.sh

--- tuyaenergy/run.sh
+++ tuyaenergy/run.sh
@@ -8,13 +8,10 @@ MQTTPORT=$(jq --raw-output ".mqttport" $CONFIG_PATH)
 MQTTUSER=$(jq --raw-output ".mqttuser" $CONFIG_PATH)
 MQTTPASSWORD=$(jq --raw-output ".mqttpassword" $CONFIG_PATH)
 MQTTTOPIC=$(jq --raw-output ".mqtttopic" $CONFIG_PATH)
-TUYAIPS=$(jq --raw-output ".tuyaips" $CONFIG_PATH)
-TUYADEVICEID=$(jq --raw-output ".tuyadeviceid" $CONFIG_PATH)
 
-
-echo "Parameters: $MQTTSERVER $MQTTSERVER $MQTTPASSWORD $MQTTTOPIC $TUYAIPS $TUYADEVICEID $MQTTPORT"
+echo "Parameters: $MQTTSERVER $MQTTUSER $MQTTPASSWORD $MQTTTOPIC $MQTTPORT"
 
 while true; do
-  python tuyaConsuption.py $MQTTSERVER $MQTTUSER $MQTTPASSWORD $MQTTTOPIC $TUYAIPS $TUYADEVICEID $MQTTPORT || true
-  sleep 30
+  python tuyaConsuption.py $MQTTSERVER $MQTTUSER $MQTTPASSWORD $MQTTTOPIC $MQTTPORT || true
+  sleep 60
 done
