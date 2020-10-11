8530661cf5991b404081741bc86d9bd51235061d
rpetteruti <raffaele.petteruti@gmail.com>
2020-10-11 15:07:25 +0200

    Update run.sh

--- tuyaenergy/run.sh
+++ tuyaenergy/run.sh
@@ -8,6 +8,10 @@ MQTTPORT=$(jq --raw-output ".mqttport" $CONFIG_PATH)
 MQTTUSER=$(jq --raw-output ".mqttuser" $CONFIG_PATH)
 MQTTPASSWORD=$(jq --raw-output ".mqttpassword" $CONFIG_PATH)
 MQTTTOPIC=$(jq --raw-output ".mqtttopic" $CONFIG_PATH)
+TUYADEVIPS=$(jq --raw-output ".tuyadevips" $CONFIG_PATH)
+TUYADEVIDS=$(jq --raw-output ".tuyadevids" $CONFIG_PATH)
+TUYADEVKEYS=$(jq --raw-output ".tuyadevkeys" $CONFIG_PATH)
+TUYADEVVERS=$(jq --raw-output ".tuyadevvers" $CONFIG_PATH)
 
 echo "Parameters: $MQTTSERVER $MQTTUSER $MQTTPASSWORD $MQTTTOPIC $MQTTPORT $TUYADEVIPS $TUYADEVIDS $TUYADEVKEYS $TUYADEVVERS"
 
