91828ce51971a9dfe7ac88cfa6bbec1b3bc915fb
R.P <>
2021-07-11 14:17:58 +0200

    aggiornati tempi

--- internetreboot/run.sh
+++ internetreboot/run.sh
@@ -9,7 +9,7 @@ echo "Parameters: $TUYAPLUGIP "
 
 #echo "nameserver ${DNS}" > /etc/resolv.conf
 echo "Wait 240s before start loop"
-sleep 240
+sleep 600
 echo "starting loop"
 while true; do
   if ping -c 10 8.8.8.8 &> /dev/null
@@ -18,11 +18,11 @@ while true; do
   else
     echo "$(date) Internet KO, starting REBOOT the modem and wait 30s"
     curl "http://$TUYAPLUGIP/cm?cmnd=Power%20Off"
-    sleep 30
+    sleep 60
     curl "http://$TUYAPLUGIP/cm?cmnd=Power%20On"
     echo "Wait 240s before checking again"
     sleep 240
     echo "restarting loop"
   fi
-  sleep 30
+  sleep 600
 done
