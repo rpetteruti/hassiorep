cf2f9e0de9f994426a7023ea26b8227b2f55908c
rpetteruti <raffaele.petteruti@gmail.com>
2022-02-06 23:11:03 +0100

    aggiunto comando backlog per tasmota
    aggiunto comando backlog per tasmota per poter spegnere e riaccedendere senza che il wifi sia ancora tornato online

--- internetreboot/run.sh
+++ internetreboot/run.sh
@@ -8,20 +8,23 @@ TUYAPLUGIP=$(jq --raw-output ".tuyaplugip" $CONFIG_PATH)
 echo "Parameters: $TUYAPLUGIP "
 
 #echo "nameserver ${DNS}" > /etc/resolv.conf
-echo "Wait 240s before start loop"
-sleep 600
+echo "Wait 1200 secs before start loop"
+sleep 1200
 echo "starting loop"
 while true; do
-  if ping -c 10 8.8.8.8 &> /dev/null
+  if ping -c 10 www.google.it &> /dev/null
   then
     echo "$(date) Internet OK"
   else
-    echo "$(date) Internet KO, starting REBOOT the modem and wait 30s"
-    curl "http://$TUYAPLUGIP/cm?cmnd=Power%20Off"
-    sleep 60
-    curl "http://$TUYAPLUGIP/cm?cmnd=Power%20On"
-    echo "Wait 240s before checking again"
-    sleep 240
+    echo "$(date) Internet KO, starting REBOOT the modem and wait 5 minutes"
+    curl "http://$TUYAPLUGIP/cm?cmnd=Backlog%20Power%20OFF%3BDelay%20600%3BPower%20ON"
+    
+    #curl "http://$TUYAPLUGIP/cm?cmnd=Power%20Off"
+    #sleep 60
+    #curl "http://$TUYAPLUGIP/cm?cmnd=Power%20On"
+    
+    echo "Wait 300 + 600 secs before checking again"
+    sleep 300
     echo "restarting loop"
   fi
   sleep 600
